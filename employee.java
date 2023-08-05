31955f0c51ae05d6c2d290e527d1baaeb6fc4e92
AsmitaSubh <saswain92@gmail.com>
2023-08-06 00:10:20 +0530

    employee

---
+++ employee.java
@@ -0,0 +1,14 @@
+CREATE TABLE Employee  
+(  
+EmployeeID int,  
+FirstName varchar(255),  
+LastName varchar(255),  
+Email varchar(255),  
+AddressLine varchar(255),  
+City varchar(255)  
+);  
+DROP TABLE "table_name"; 
+TRUNCATE TABLE employee;
+DELETE FROM table_name;  
+RENAME old_table _name To new_table_name ;  
+USE database_name;  
